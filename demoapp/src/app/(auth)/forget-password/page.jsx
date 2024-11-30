0516970efc352ef5788602b8baf428c60690ac39
rajarya199 <aryalrajan078@gmail.com>
2024-11-30 21:40:10 +0545

    group route

---
+++ demoapp/src/app/(auth)/forget-password/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div> Forget password</div>
+  )
+}
+
+export default page
---
+++ demoapp/src/app/(auth)/register/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div> register now</div>
+  )
+}
+
+export default page
