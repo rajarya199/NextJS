226d5f9f5e2e6e7d53d3dea8908d74ac589ee0b5
rajarya199 <aryalrajan078@gmail.com>
2024-12-07 23:05:37 +0545

    pathname/ active link

--- demoapp/src/app/components/navigation.jsx
+++ demoapp/src/app/components/navigation.jsx
@@ -1,15 +1,19 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
-const navigation = () => {
+import { usePathname } from 'next/navigation'
+const Navigation = () => {
+    const pathname=usePathname();
+
   return (
     <nav>
-        <Link href="/" className='mr-4 text-blue-500'>
+        <Link href="/" className={pathname==="/" ? "font-bold mr-4":" text-blue-500 mr-4"}>
         Home
         </Link>
-        <Link href="/about" className='mr-4 text-blue-500'>
+        <Link href="/about" className={pathname==="/about" ? "font-bold mr-4":" text-blue-500 mr-4"}>
         About
         </Link>
-        <Link href="/products/tv" className='mr-4 text-blue-500'>
+        <Link href="/products/tv" className={pathname.startsWith("/products/tv") ? "font-bold mr-4":" text-blue-500 mr-4"}>
             Product1 
         </Link>
 
@@ -18,4 +22,4 @@ const navigation = () => {
   )
 }
 
-export default navigation
+export default Navigation
