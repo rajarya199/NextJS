a9cf84701f401e7e903754aad5c452654dddc39f
rajarya199 <aryalrajan078@gmail.com>
2024-12-07 23:13:58 +0545

    useRouter

--- demoapp/src/app/about/page.jsx
+++ demoapp/src/app/about/page.jsx
@@ -1,8 +1,20 @@
+"use client"
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const About = () => {
+  const router=useRouter()
   return (
-    <div>About us page</div>
+    <div>
+      <h1>
+      About us page
+        </h1>
+        <button onClick={()=>router.push('/')}
+        className='bg-blue-500 text-white p-3 ml-2 rounded-md'>
+            Go to HomePage
+        </button>
+        
+        </div>
   )
 }
 
